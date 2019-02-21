ab775536d48ea96720527ae2e623bb5a44bf1db7
LetsStudyTogether <1050361636@qq.com>
2019-02-22 00:16:42 +0800

    可以抢多个课程的多个分组（如果有的话）

--- clicker.js
+++ clicker.js
@@ -2,25 +2,15 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
 var sleeptime=3500;
+if(true)
+	sleeptime=Math.ceil(Math.random()*5000)+1000;
 
-async function mainfunc(sleeptime)	{
-	// 把inputid的值修改为你想选的课号，会自动处理。
-	var inputid1= "1296";
-	var inputid2 = "1369";
-	var a = Math.ceil(Math.random()*10); 
+var inputid=new Array("0161","0175");
 
-	var inputid;
-	if (a % 2 == 1) {
-		inputid = inputid1;
-	}else {
-		inputid = inputid2;
-	}
-
-	// var sleeptime = a * 1000;
-
-	$("input[name='electableLesson.no']").val(inputid);
+async function mainfunc(sleeptime,inputid)	{
+	var id=inputid[Math.floor(Math.random()*inputid.length)];
+	$("input[name='electableLesson.no']").val(id);
 	$("#electableLessonList_filter_submit").click();
 	var targetid=$("tr[class='electGridTr electGridTr-even']").attr('id');
 	var shortid=targetid.substr(6,6);
@@ -28,16 +18,15 @@ async function mainfunc(sleeptime)	{
 	var groupid;
 	if($(expLesson).attr('value'))	{
 		groupid=$(expLesson).attr('value');
+		var expLessonArray=document.getElementsByName("expLessonGroups_"+targetid.substr(6,6));
+		if (expLessonArray.length>1)	{
+			var lessonRandom=Math.floor(Math.random()*expLessonArray.length);
+			groupid=$(expLessonArray[lessonRandom]).attr('value');
+		}
 	}
 	else 	{
 		groupid="undefined";
 	}
-	/* 
-	* 如果你不想选默认的第一个组，那么把groupid替换成想选的组对应四位编号。
-	* 四位编号是html中想选的组名的上一个input标签的value值。
-	* 填写不正确的组号会带来不可预知的后果！！！
-	groupid=2470;
-	*/
 	var getdata;
 	var request=$.ajax({
 	  type: "POST",
@@ -52,11 +41,21 @@ async function mainfunc(sleeptime)	{
 	});
 	await sleep(sleeptime);
 	if(getdata.indexOf("成功")>=0) 	{
-		alert("success!");
-		return;
+		if(inputid.length>1)	{
+			inputid.splice($.inArray(id,inputid),1);
+			console.log("Success in "+id+" Group:"+groupid);
+			mainfunc(sleeptime,inputid);
+		}
+		else {
+			console.log("Success in "+id+" Group:"+groupid);
+			alert("All Success!");
+			return;
+		}
 	}
-	console.log("Failed...Try again... " + inputid);
-	mainfunc(a * 1000);
+	else 	{
+		console.log("Failed...Try again...Last try in "+id+" groupid: "+groupid);
+		mainfunc(sleeptime,inputid);
+	}
 }
 
-mainfunc(sleeptime);
+mainfunc(sleeptime,inputid);
