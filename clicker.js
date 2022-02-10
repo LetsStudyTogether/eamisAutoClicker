4d81ca5abea8541244b2c2f59655658eacacda1f
小辣稽 <xuyunkai@mail.nankai.edu.cn>
2022-02-10 13:53:38 +0800

    Update clicker.js
    auto detect profileId

--- clicker.js
+++ clicker.js
@@ -1,22 +1,29 @@
+// 把下面的数组元素修改为你想选的课程序号（不是课程代码，是选课页面最左侧那一列的数值）
+// 注意格式，每个课程序号用英文单引号括起来，多个课程之间用英文逗号分割。比如这里是要选课0416文科概率统计和0428高等数学（B类）II
+const courseId = ['0416', '0428'];
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// 把下面的数组元素修改为你想选的课程编号
-const courseId = ['1108', '1434'];
-// 在这里修改选课系统ID
-const profileId = '834';
-
 async function mainFunction(courseId) {
+  const eamisUrl = window.location.href
+  const profileId = eamisUrl.substring(eamisUrl.search(/[0-9]+/), eamisUrl.length)
+  
   let idInd = 0;
   while (courseId.length > 0) {
-    // 在这里修改随机等待时长
+    // 如有需要，在这里修改随机等待时长的计算规则（不建议修改）
     const sleepTime = Math.ceil(Math.random() * 200) + 300;
     const id = courseId[idInd++];
     idInd %= courseId.length;
     $("input[name='electableLesson.no']").val(id);
     $('#electableLessonList_filter_submit').click();
     const targetId = $("tr[class='electGridTr electGridTr-even']").attr('id');
+    if (targetId == null) {
+      courseId.splice($.inArray(id, courseId), 1);
+      console.log(`Failed... Id: ${id}, course unavailable!`);
+      continue;
+    }
     const shortId = targetId.substr(6, 6);
     const expLesson = `#expLessonGroups_${targetId.substr(6, 6)}`;
     let groupId;
